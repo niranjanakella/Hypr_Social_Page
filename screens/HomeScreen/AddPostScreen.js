40d68ab51f6eebd17134c6562f15f31ce7270510
niranjanstudy06 <niranjanstudy06@gmail.com>
2022-08-18 13:34:02 +0530

    yes

--- screens/HomeScreen/AddPostScreen.js
+++ screens/HomeScreen/AddPostScreen.js
@@ -1,5 +1,5 @@
-import { View, Text, SafeAreaView } from "react-native";
-import React from "react";
+import {View, Text, SafeAreaView} from 'react-native';
+import React from 'react';
 
 const AddPostScreen = () => {
   return (
